be4e8a02c3b134bbfec52e563c164b65d86ad52d
Aledax <dalexay16@gmail.com>
2023-11-12 19:32:26 -0800

    completed

--- scripts/rain.gd
+++ scripts/rain.gd
@@ -2,5 +2,12 @@ extends Sprite2D
 
 @export var fall_speed = 1800
 
+func _ready():
+	var screen_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+	var screen_height = ProjectSettings.get_setting("display/window/size/viewport_height")
+	var screen_size = Vector2(screen_width, screen_height)
+	position = screen_size / 2
+	region_rect.size = screen_size * 1.5
+
 func _process(delta):
 	region_rect.position.y -= fall_speed * delta
