56a154c1bf6768eb609ba84531620e48d128626f
Aledax <dalexay16@gmail.com>
2023-11-17 00:26:27 -0800

    finished kousa movements

--- scripts/world.gd
+++ scripts/world.gd
@@ -5,12 +5,23 @@ const npc_move_event = "_npc_move_event"
 const npc_jump_event = "_npc_jump_event"
 const npc_switch_dialogue_event = "_npc_switch_dialogue_event"
 
+const kousa_beach_start = 45
+
 func _ready():
-	schedule_event("Ocean", ocean_rise_event, 1, [5])
+	schedule_event("Ocean", ocean_rise_event, 120, [5])
 	
 	schedule_event("NPCs", npc_move_event, 1, ["sorrel", -50])
 	schedule_event("NPCs", npc_jump_event, 2, ["sorrel", 0.1])
 	schedule_event("NPCs", npc_switch_dialogue_event, 5, ["sorrel", "test"])
+	
+	# Kousa
+	schedule_event("NPCs", npc_move_event, kousa_beach_start + 0, ["kousa", -340])
+	schedule_event("NPCs", npc_move_event, kousa_beach_start + 9, ["kousa", -290])
+	schedule_event("NPCs", npc_move_event, kousa_beach_start + 10, ["kousa", -370])
+	schedule_event("NPCs", npc_move_event, kousa_beach_start + 12, ["kousa", 0])
+	schedule_event("NPCs", npc_move_event, kousa_beach_start + 17, ["kousa", -160])
+	schedule_event("NPCs", npc_move_event, kousa_beach_start + 19, ["kousa", 400])
+	schedule_event("NPCs", npc_jump_event, kousa_beach_start + 22.2, ["kousa", 0.1])
 
 func schedule_event(event_group, event_name, time_start, event_args):
 	print("Event: ", event_name, " will be called in ", time_start, " seconds")
