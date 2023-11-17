45f365cc2f7ba1a7d52d7f58a4bcfa2ed38b743f
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2023-11-17 02:51:05 -0800

    Created falling roof

---
+++ scenes/restaurant_collapse.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var tiles
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	tiles = get_children()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+# Call this using an event from world
+func collapse_tiles():
+	for tile in tiles:
+		if tile is PhysicsBody2D:
+			tile.gravity_scale = 0.3
+			tile.linear_velocity.x += 100
+	#		tile.linear_velocity.y += 100
+
