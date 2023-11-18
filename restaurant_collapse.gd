db666d41bac2151348b86042b81190e233e3225e
Munn Chai <113135503+LotsaChai@users.noreply.github.com>
2023-11-17 16:16:13 -0800

    Created outlook falling

--- restaurant_collapse.gd
+++ restaurant_collapse.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
+@export var apply_torque = 0
 var tiles
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tiles = get_children()
-#	collapse_tiles()
+	collapse_tiles()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,8 +18,9 @@ func _process(delta):
 func collapse_tiles():
 	for tile in tiles:
 		if tile is PhysicsBody2D:
+#			await get_tree().create_timer(2).timeout
 			tile.gravity_scale = 0.3
-#			tile.angular_velocity += 
-			tile.linear_velocity.x += 0
+			tile.angular_velocity += apply_torque
+			tile.linear_velocity.x += apply_torque / 5
 			tile.linear_velocity.y -= 80
 
